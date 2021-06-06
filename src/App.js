502e30d31baf3cd824ee790b112cf6c672789cb7
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-06-05 21:20:12 -0300

    Create Snapshot from valtio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Suspense, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
-import { proxy, useProxy } from 'valtio'
+import { proxy, useSnapshot } from "valtio"
 
 // Using a Valtio state model to bridge reactivity between
 // the canvas and the dom, both can write to it and/or react to it.
@@ -15,23 +15,25 @@ const state = proxy({
     sole: "#ffffff",
     stripes: "#ffffff",
     band: "#ffffff",
-    patch: "#ffffff",
+    patch: "#ffffff"
   },
 })
+
 function Shoe(props) {
   const group = useRef() 
+  const snap = useSnapshot(state)
   const { nodes, materials } = useGLTF("shoe-draco.glb")
   
   return (
     <group ref={group} {...props} dispose={null}>
-      <mesh material={materials.laces} geometry={nodes.shoe.geometry} />
-      <mesh material={materials.mesh} geometry={nodes.shoe_1.geometry} />
-      <mesh material={materials.caps} geometry={nodes.shoe_2.geometry} />
-      <mesh material={materials.inner} geometry={nodes.shoe_3.geometry} />
-      <mesh material={materials.sole} geometry={nodes.shoe_4.geometry} />
-      <mesh material={materials.stripes} geometry={nodes.shoe_5.geometry} />
-      <mesh material={materials.band} geometry={nodes.shoe_6.geometry} />
-      <mesh material={materials.patch} geometry={nodes.shoe_7.geometry} />
+      <mesh material-color={snap.items.laces} material={materials.laces} geometry={nodes.shoe.geometry} />
+      <mesh material-color={snap.items.mesh} material={materials.mesh} geometry={nodes.shoe_1.geometry} />
+      <mesh material-color={snap.items.caps} material={materials.caps} geometry={nodes.shoe_2.geometry} />
+      <mesh material-color={snap.items.inner} material={materials.inner} geometry={nodes.shoe_3.geometry} />
+      <mesh material-color={snap.items.sole} material={materials.sole} geometry={nodes.shoe_4.geometry} />
+      <mesh material-color={snap.items.stripes} material={materials.stripes} geometry={nodes.shoe_5.geometry} />
+      <mesh material-color={snap.items.band} material={materials.band} geometry={nodes.shoe_6.geometry} />
+      <mesh material-color={snap.items.patch} material={materials.patch} geometry={nodes.shoe_7.geometry} />
     </group>
   );
 }
