67f836cbf64a268b6259625f4ff2002f6c911dda
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-06-05 21:12:48 -0300

    Install valtio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
 import { proxy, useProxy } from 'valtio'
 
+// Using a Valtio state model to bridge reactivity between
+// the canvas and the dom, both can write to it and/or react to it.
+const state = proxy({
+  current: null,
+  items: {
+    laces: "#ffffff",
+    mesh: "#ffffff",
+    caps: "#ffffff",
+    inner: "#ffffff",
+    sole: "#ffffff",
+    stripes: "#ffffff",
+    band: "#ffffff",
+    patch: "#ffffff",
+  },
+})
 function Shoe(props) {
   const group = useRef() 
   const { nodes, materials } = useGLTF("shoe-draco.glb")
