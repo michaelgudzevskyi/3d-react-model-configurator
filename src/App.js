f659777659b83d0e34d614944ea9cc65d6064f48
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-06-05 21:57:31 -0300

    Add Contact Shadows

--- src/App.js
+++ src/App.js
@@ -26,6 +26,16 @@ function Shoe(props) {
   const snap = useSnapshot(state)
   const { nodes, materials } = useGLTF("shoe-draco.glb")
 
+  
+  // Animate model
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    group.current.rotation.z = -0.2 - (1 + Math.sin(t / 1.5)) / 20
+    group.current.rotation.x = Math.cos(t / 4) / 8
+    group.current.rotation.y = Math.sin(t / 4) / 8
+    group.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+  })
+
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
@@ -71,11 +81,21 @@ function App() {
   return (
     <>
       <Picker />
-      <Canvas>
+      <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
         <ambientLight intensity={0.3} />
         <spotLight intensity={0.3} angle={0.1} penumbra={1} position={[5, 25, 20]} />
         <Suspense fallback={null}>
           <Shoe />
+          <Environment preset="city" />
+          <ContactShadows 
+            rotation-x={Math.PI / 2} 
+            position={[0, -0.8, 0]} 
+            opacity={0.25} 
+            width={10} 
+            height={10} 
+            blur={1.5} 
+            far={0.8} 
+          />
         </Suspense>
         <OrbitControls />
       </Canvas>
